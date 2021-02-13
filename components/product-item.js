d246bb8dfde9caf1122a4b1913aec42fcab2c7f9
Ethan Huynh <e2huynh@ucsd.edu>
2021-02-12 23:41:07 -0800

    Finished Lab6

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,100 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  #title;
+  #price;
+  #image;
+  #id;
+  #shadow;
+  constructor(titleStr, priceStr, imageStr, idNum) {
+    super();
+
+    this.#title = titleStr;
+    this.#price = priceStr;
+    this.#image = imageStr;
+    this.#id = idNum;
+
+    var shadowRoot = this.attachShadow({mode: 'open'});
+    this.#shadow = shadowRoot;
+
+    shadowRoot.innerHTML = `
+      <li class="product">
+        <link rel="stylesheet" href="./styles/styles.css">
+        <img src="` + imageStr + `" alt="` + titleStr + `" width=200 height=286>
+        <p class="title">` + titleStr + `</p>
+        <p class="price">` + priceStr + `</p>
+        <button onclick="alert('Added to Cart!')">Add to Cart</button>
+      </li>
+    `;
+
+    this.addEventListener("click", function() {
+      if(shadowRoot.innerHTML.indexOf("Add to Cart") != -1) {
+        //change to remove cart button
+        shadowRoot.innerHTML = `
+          <li class="product">
+            <link rel="stylesheet" href="./styles/styles.css">
+            <img src="` + imageStr + `" alt="` + titleStr + `" width=200 height=286>
+            <p class="title">` + titleStr + `</p>
+            <p class="price">` + priceStr + `</p>
+            <button onclick="alert('Removed from Cart!')">Remove from Cart</button>
+          </li>
+        `;
+
+        //increase the number of items in the cart by 1
+        var cartNum = document.getElementById("cart-count");
+        cartNum.innerHTML = Number(cartNum.innerHTML) + 1;
+
+        //add item to local storage
+        localStorage.setItem("cart" + idNum, idNum);
+      }
+      else {
+        //change to add cart button
+        shadowRoot.innerHTML = `
+          <li class="product">
+            <link rel="stylesheet" href="./styles/styles.css">
+            <img src="` + imageStr + `" alt="` + titleStr + `" width=200 height=286>
+            <p class="title">` + titleStr + `</p>
+            <p class="price">` + priceStr + `</p>
+            <button onclick="alert('Added to Cart!')">Add to Cart</button>
+          </li>
+        `;
+
+        //decrease the number of items in the cart by 1
+        var cartNum = document.getElementById("cart-count");
+        cartNum.innerHTML = Number(cartNum.innerHTML) - 1;
+
+        //remove item from local storage
+        localStorage.removeItem("cart" + idNum);
+      }
+    });
+  }
+
+  setRemoveBtn() {
+    //change to remove cart button
+    this.#shadow.innerHTML = `
+      <li class="product">
+        <link rel="stylesheet" href="./styles/styles.css">
+        <img src="` + this.#image + `" alt="` + this.#title + `" width=200 height=286>
+        <p class="title">` + this.#title + `</p>
+        <p class="price">` + this.#price + `</p>
+        <button onclick="alert('Removed from Cart!')">Remove from Cart</button>
+      </li>
+    `;
+  }
+  
+  //this function is not used but might be useful in future lab?
+  setAddBtn() {
+    //change to add cart button
+    shadow.innerHTML = `
+      <li class="product">
+        <link rel="stylesheet" href="./styles/styles.css">
+        <img src="` + this.#image + `" alt="` + this.#title + `" width=200 height=286>
+        <p class="title">` + this.#title + `</p>
+        <p class="price">` + this.#price + `</p>
+        <button onclick="alert('Added to Cart!')">Add to Cart</button>
+      </li>
+    `;
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,36 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  if(localStorage.length == 0) {
+    fetch("https://fakestoreapi.com/products")
+      .then(response => response.json())
+      .then(data => {
+        for(var i = 0; i < data.length; i++)
+          localStorage.setItem((i+1).toString(), JSON.stringify(data[i]));
+        
+      });
+  }
+  var list = document.getElementById("product-list");
+  for(var x = 1; x <= 20; x++) {
+    var itemToLoad = localStorage.getItem(x.toString());
+    var titleIndex = itemToLoad.indexOf("\"title\":");
+    var priceIndex = itemToLoad.indexOf("\"price\":");
+    var descriptionIndex = itemToLoad.indexOf("\"description\":");
+    var imageIndex = itemToLoad.indexOf("\"image\":");
+
+    var strTitle = itemToLoad.substring(titleIndex + 9, priceIndex - 2);
+    var strPrice = itemToLoad.substring(priceIndex + 8, descriptionIndex - 1);
+    var strImage = itemToLoad.substring(imageIndex + 9, itemToLoad.length - 2);
+    var item = new ProductItem(strTitle, strPrice, strImage, x);
+
+    if(localStorage.getItem("cart" + x)) {
+      item.setRemoveBtn();
+      //increase the number of items in the cart by 1
+      var cartNum = document.getElementById("cart-count");
+      cartNum.innerHTML = Number(cartNum.innerHTML) + 1;
+    }
+  
+    list.appendChild(item);
+  }
+  
 });
