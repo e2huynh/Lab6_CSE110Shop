5636b6b267f826df1ea06b4ae3b42fc1c4c08c94
Ethan Huynh <e2huynh@ucsd.edu>
2021-02-13 01:01:38 -0800

    Fixed async loading for non-first time viewers

--- scripts/script.js
+++ scripts/script.js
@@ -32,4 +32,27 @@ window.addEventListener('DOMContentLoaded', () => {
           }
       });
   }
+
+  var list = document.getElementById("product-list");
+  for(var x = 1; x <= 20; x++) {
+    var itemToLoad = localStorage.getItem(x.toString());
+    var titleIndex = itemToLoad.indexOf("\"title\":");
+    var priceIndex = itemToLoad.indexOf("\"price\":");
+    var descriptionIndex = itemToLoad.indexOf("\"description\":");
+    var imageIndex = itemToLoad.indexOf("\"image\":");
+
+    var strTitle = itemToLoad.substring(titleIndex + 9, priceIndex - 2);
+    var strPrice = itemToLoad.substring(priceIndex + 8, descriptionIndex - 1);
+    var strImage = itemToLoad.substring(imageIndex + 9, itemToLoad.length - 2);
+    var item = new ProductItem(strTitle, strPrice, strImage, x);
+
+    if(localStorage.getItem("cart" + x)) {
+      item.setRemoveBtn();
+      //increase the number of items in the cart by 1
+      var cartNum = document.getElementById("cart-count");
+      cartNum.innerHTML = Number(cartNum.innerHTML) + 1;
+    }
+
+    list.appendChild(item);
+  }
 });
