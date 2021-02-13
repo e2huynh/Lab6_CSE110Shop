f1816fbaec1c0c28b34db65288d72b4302b4f059
Ethan Huynh <e2huynh@ucsd.edu>
2021-02-13 04:35:15 -0800

    A little bit of polish

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
+  //if local storage is empty, load in products
   if(localStorage.length == 0) {
     fetch("https://fakestoreapi.com/products")
       .then(response => response.json())
@@ -32,27 +33,28 @@ window.addEventListener('DOMContentLoaded', () => {
           }
       });
   }
+  else {
+    var list = document.getElementById("product-list");
+    for(var x = 1; x <= 20; x++) {
+      var itemToLoad = localStorage.getItem(x.toString());
+      var titleIndex = itemToLoad.indexOf("\"title\":");
+      var priceIndex = itemToLoad.indexOf("\"price\":");
+      var descriptionIndex = itemToLoad.indexOf("\"description\":");
+      var imageIndex = itemToLoad.indexOf("\"image\":");
 
-  var list = document.getElementById("product-list");
-  for(var x = 1; x <= 20; x++) {
-    var itemToLoad = localStorage.getItem(x.toString());
-    var titleIndex = itemToLoad.indexOf("\"title\":");
-    var priceIndex = itemToLoad.indexOf("\"price\":");
-    var descriptionIndex = itemToLoad.indexOf("\"description\":");
-    var imageIndex = itemToLoad.indexOf("\"image\":");
+      var strTitle = itemToLoad.substring(titleIndex + 9, priceIndex - 2);
+      var strPrice = itemToLoad.substring(priceIndex + 8, descriptionIndex - 1);
+      var strImage = itemToLoad.substring(imageIndex + 9, itemToLoad.length - 2);
+      var item = new ProductItem(strTitle, strPrice, strImage, x);
 
-    var strTitle = itemToLoad.substring(titleIndex + 9, priceIndex - 2);
-    var strPrice = itemToLoad.substring(priceIndex + 8, descriptionIndex - 1);
-    var strImage = itemToLoad.substring(imageIndex + 9, itemToLoad.length - 2);
-    var item = new ProductItem(strTitle, strPrice, strImage, x);
+      if(localStorage.getItem("cart" + x)) {
+        item.setRemoveBtn();
+        //increase the number of items in the cart by 1
+        var cartNum = document.getElementById("cart-count");
+        cartNum.innerHTML = Number(cartNum.innerHTML) + 1;
+      }
 
-    if(localStorage.getItem("cart" + x)) {
-      item.setRemoveBtn();
-      //increase the number of items in the cart by 1
-      var cartNum = document.getElementById("cart-count");
-      cartNum.innerHTML = Number(cartNum.innerHTML) + 1;
+      list.appendChild(item);
     }
-
-    list.appendChild(item);
   }
 });
